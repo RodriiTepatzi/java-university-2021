7523914f7a4429622b6501da6394a63af83f7a01
RodriiTepatzi <72983645+RodriiTepatzi@users.noreply.github.com>
2021-09-25 22:52:54 -0500

    Ejercicio 2

---
+++ Fundamentos/Ejercicio 2/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var miVariable = 1;
+        var _miVariable = 2;
+        var $miVariable = 3;
+
+        // var áVariable = 10; No se recomienda
+        // var #miVariable = 10; No se permite utilizar caracteres especiales
+    }
+}
