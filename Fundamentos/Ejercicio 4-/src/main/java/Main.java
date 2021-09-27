6abae27b09c25e19b6b70a446abcfad6760c8d76
RodriiTepatzi <72983645+RodriiTepatzi@users.noreply.github.com>
2021-09-27 11:44:49 -0500

    Ejercicio 4

---
+++ Fundamentos/Ejercicio 4-/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var nombre = "Nombre";
+        System.out.println("Nueva linea: \n" + nombre);
+        System.out.println("Tabulador \t" + nombre);
+        System.out.println("Retroceso: \b\b" + nombre);
+        System.out.println("Comilla simple: \'" + nombre + "'");
+        System.out.println("Comilla doble \"" + nombre + "\"");
+    }
+}
